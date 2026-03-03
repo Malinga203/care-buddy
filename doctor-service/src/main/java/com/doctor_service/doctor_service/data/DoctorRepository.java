d3942eb4f055861c4ee265718f1a8e9adcf5f049
Malinga Lakmal <malingalakmal2003@gmail.com>
2026-03-03 13:53:46 +0530

    delete backend

--- doctor-service/src/main/java/com/doctor_service/doctor_service/data/DoctorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.doctor_service.doctor_service.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepository extends JpaRepository<Doctor,Integer> {
-}
